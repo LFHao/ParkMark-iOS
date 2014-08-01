719d29fe3c359997a879382154b7b203e076458a
LFHao <hlfshiren@gmail.com>
2014-08-01 16:15:51 -0400

    Add upload function to parse

--- ParkMark-iOS/ParkMark-iOS/ParkViewController.h
+++ ParkMark-iOS/ParkMark-iOS/ParkViewController.h
@@ -7,15 +7,31 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+#import "MBProgressHUD.h"
+#import <MapKit/MapKit.h>
 
-@interface ParkViewController : UIViewController<UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIPickerViewDelegate, UIPickerViewDataSource>
+
+@interface ParkViewController : UIViewController<UIImagePickerControllerDelegate, UINavigationControllerDelegate, MBProgressHUDDelegate, CLLocationManagerDelegate>
+{
+    //IBOutlet UIScrollView *photoScrollView;
+    //NSMutableArray *allImages;
+    
+    MBProgressHUD *HUD;
+}
+
+@property (strong, nonatomic) CLLocationManager *locationManager;
+
 
 - (IBAction)takePhoto:(id)sender;
 
 - (IBAction)addNote:(id)sender;
 
+- (IBAction)addLocation:(id)sender;
+
 //- (IBAction)addAlarm:(id)sender;
 
+- (IBAction)mark:(id)sender;
 
 
 @end
--- ParkMark-iOS/ParkMark-iOS/ParkViewController.m
+++ ParkMark-iOS/ParkMark-iOS/ParkViewController.m
@@ -9,6 +9,11 @@
 #import "ParkViewController.h"
 
 @interface ParkViewController ()
+{
+    PFFile *imageFile;
+    NSString *note;
+    PFGeoPoint *point;
+}
 
 @end
 
@@ -28,6 +33,15 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    _locationManager = [[CLLocationManager alloc] init];
+    _locationManager.delegate = self;
+    _locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
     
     if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
         
@@ -41,23 +55,87 @@
     }
 }
 
-- (void)didReceiveMemoryWarning
+/******************************************Take photo*****************************************************/
+
+- (IBAction)takePhoto:(id)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    // Create image picker controller
+    UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+    // Set source to the camera
+    imagePicker.sourceType =  UIImagePickerControllerSourceTypeCamera;
+    // Delegate is self
+    imagePicker.delegate = self;
+    // Show image picker
+    [self presentViewController:imagePicker animated:YES completion:nil];
 }
 
-- (IBAction)takePhoto:(UIButton *)sender
+- (void)uploadImage:(NSData *)imageData
 {
+    imageFile = [PFFile fileWithName:@"Image.jpg" data:imageData];
     
-    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
-    picker.delegate = self;
-    picker.allowsEditing = YES;
-    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    HUD = [[MBProgressHUD alloc] initWithView:self.view];
+    [self.view addSubview:HUD];
     
-    [self presentViewController:picker animated:YES completion:NULL];
+    // Set determinate mode
+    HUD.mode = MBProgressHUDModeDeterminate;
+    HUD.delegate = self;
+    HUD.labelText = @"Uploading";
+    [HUD show:YES];
     
+    // Save PFFile
+    [imageFile saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (!error) {
+            //Hide determinate HUD
+            [HUD hide:YES];
+            
+            // Show checkmark
+            HUD = [[MBProgressHUD alloc] initWithView:self.view];
+            [self.view addSubview:HUD];
+            
+            // The sample image is based on the work by http://www.pixelpressicons.com, http://creativecommons.org/licenses/by/2.5/ca/
+            // Make the customViews 37 by 37 pixels for best results (those are the bounds of the build-in progress indicators)
+            HUD.customView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"37x-Checkmark.png"]];
+            
+            // Set custom view mode
+            HUD.mode = MBProgressHUDModeCustomView;
+            HUD.delegate = self;
+        }
+        else{
+            [HUD hide:YES];
+            // Log details of the failure
+            NSLog(@"Error: %@ %@", error, [error userInfo]);
+        }
+    } progressBlock:^(int percentDone) {
+        // Update your progress spinner here. percentDone will be between 0 and 100.
+        HUD.progress = (float)percentDone/100;
+    }];
 }
+
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    // Access the uncropped image from info dictionary
+    UIImage *image = [info objectForKey:@"UIImagePickerControllerOriginalImage"];
+    
+    // Dismiss controller
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    
+    // Resize image
+    UIGraphicsBeginImageContext(CGSizeMake(640, 960));
+    [image drawInRect: CGRectMake(0, 0, 640, 960)];
+    UIImage *smallImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    // Upload image
+    NSData *imageData = UIImageJPEGRepresentation(smallImage, 0.05f);
+    [self uploadImage:imageData];
+}
+
+
+/******************************************END of take photo*****************************************************/
+
+
+/********************************************Add note********************************************************/
 
 - (IBAction)addNote:(id)sender
 {
@@ -72,60 +150,56 @@
     [alert show];
 }
 
-//- (IBAction)addAlarm:(id)sender {
-//    UIAlertView *alert = [[UIAlertView alloc]initWithTitle: @"Add Note"
-//                                                   message: @"Please enter your parking info here"
-//                                                  delegate: self
-//                                         cancelButtonTitle:@"Cancel"
-//                                         otherButtonTitles:@"Add", nil];
-//    alert.alertViewStyle = UIAlertViewStylePlainTextInput;
-//    
-//    // create a UIPicker view as a custom keyboard view
-//    UIPickerView* pickerView = [[UIPickerView alloc] init];
-//    [pickerView sizeToFit];
-//    pickerView.autoresizingMask = (UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight);
-//    pickerView.delegate = self;
-//    pickerView.dataSource = self;
-//    pickerView.showsSelectionIndicator = YES;
-//    
-//    UITextField * alertTextField = [alert textFieldAtIndex:0];
-//    alertTextField.inputView = pickerView;
-//    
-//    [alert show];
-//    
-//}
+
 
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
-    NSLog(@"Entered: %@",[[alertView textFieldAtIndex:0] text]);
+    note = [[alertView textFieldAtIndex:0] text];
+}
+
+/******************************************END of add note*****************************************************/
+
+
+/******************************************Add location*******************************************************/
+
+- (IBAction)addLocation:(id)sender
+{
+    [_locationManager startUpdatingLocation];
+    point = [PFGeoPoint geoPointWithLatitude:_locationManager.location.coordinate.latitude
+                                               longitude:_locationManager.location.coordinate.longitude];
 }
 
 
 
-//- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
-//    
-//    UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
-//    imageView.image = chosenImage;
-//    
-//    [picker dismissViewControllerAnimated:YES completion:NULL];
-//    
-//}
 
-- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
-    
-    [picker dismissViewControllerAnimated:YES completion:NULL];
-    
+/******************************************End of add location*******************************************************/
+
+
+
+/******************************************Mark*******************************************************/
+
+
+- (IBAction)mark:(id)sender
+{
+    PFObject *record = [PFObject objectWithClassName:@"ParkHistory"];
+    record[@"note"] = note;
+    record[@"image"] = imageFile;
+    record[@"geoLocation"] = point;
+    [record saveInBackground];
 }
 
+
+/******************************************End of mark*******************************************************/
 
 
 /*
-#pragma mark - Navigation
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+ {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 @end
